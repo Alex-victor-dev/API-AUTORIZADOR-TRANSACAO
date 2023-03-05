308b2280473ff80e8bfbcdd5ebacdd1520fabc6b
Alex-Victor <alexvictoribeiro@gmail.com>
2023-03-05 20:17:15 -0300

    implementa camada  de infra

--- src/main/java/br/com/autorizador/cartaotransacao/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/autorizador/cartaotransacao/cliente/infra/ClienteInfraRepository.java
@@ -3,12 +3,23 @@ package br.com.autorizador.cartaotransacao.cliente.infra;
 import org.springframework.stereotype.Repository;
 
 import br.com.autorizador.cartaotransacao.cliente.application.repository.ClienteRepository;
+import br.com.autorizador.cartaotransacao.cliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Repository
 @RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
 
 	private final ClienteInfraJpaRepository clienteInfraJpaRepository;
+
+	@Override
+	public Cliente salvaCliente(Cliente cliente) {
+		log.info("[Start] ClienteInfraRepository - salvaCliente");
+		clienteInfraJpaRepository.save(cliente);
+		log.info("[Finish] ClienteInfraRepository - salvaCliente");
+		return cliente;
+	}
 
 }
